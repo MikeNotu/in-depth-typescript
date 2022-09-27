5b7ce5f99126d40db597e029eab8dd7e2173e9d5
MikeNotu <michael.bateman@globant.com>
2022-09-27 14:04:33 -0500

    Enums commented

--- src/components/EnumsComponent.tsx
+++ src/components/EnumsComponent.tsx
@@ -51,22 +51,45 @@ const EnumsComponent = () => {
 
   return (
     <div>
-      <p>{`${JSON.stringify(FirstPrices)}`}</p>
-      <p>{`${FirstPrices[0]} ${FirstPrices[1]} ${FirstPrices[2]}`}</p>
-      <p>{`${FirstPrices.applePrice} ${FirstPrices.bananaPrice} ${FirstPrices.peachPrice}`}</p>
-      <p>{`${SecondPrices.applePrice} ${SecondPrices.bananaPrice} ${SecondPrices.peachPrice}`}</p>
-      <p>{`${ThirdPrices.applePrice} ${ThirdPrices.bananaPrice} ${ThirdPrices.peachPrice}`}</p>
-      <p>{`${FourthPrices.applePrice} ${FourthPrices.bananaPrice} ${FourthPrices.peachPrice}`}</p>
-      <p>{`${FifthPrices.applePrice} ${FifthPrices.bananaPrice} ${FifthPrices.peachPrice}`}</p>
-      <p>{`${JSON.stringify(Direction)}`}</p>
       <p>TypeScript compiles regular enum to JavaScript objects.</p>
+      <p>Displaying Prices Enum</p>
+      <p>{`${JSON.stringify(FirstPrices)}`}</p>
+      <br />
+      <p>Prices[0] Prices[1] Prices[2]</p>
+      <p>{`${FirstPrices[0]} ${FirstPrices[1]} ${FirstPrices[2]}`}</p>
+      <br />
+      <p>Prices.applePrice Prices.bananaPrice Prices.peachPrice</p>
+      <p>{`${FirstPrices.applePrice} ${FirstPrices.bananaPrice} ${FirstPrices.peachPrice}`}</p>
+      <br />
+      <p>applePrice = 4</p>
+      <p>Prices.applePrice Prices.bananaPrice Prices.peachPrice </p>
+      <p>{`${SecondPrices.applePrice} ${SecondPrices.bananaPrice} ${SecondPrices.peachPrice}`}</p>
+      <br />
+      <p>
+        applePrice = 10, bananaPrice = triplePrice(11), peachPrice = 5 *
+        applePrice
+      </p>
+      <p>Prices.applePrice Prices.bananaPrice Prices.peachPrice </p>
+      <p>{`${ThirdPrices.applePrice} ${ThirdPrices.bananaPrice} ${ThirdPrices.peachPrice}`}</p>
+      <br />
+      <p>Const Enum</p>
       <p>
         Because there is no JavaScript object that associates with const enum is
         generated at run time, it is not possible to loop over the const enum
         values.
       </p>
+      <p>{`${FourthPrices.applePrice} ${FourthPrices.bananaPrice} ${FourthPrices.peachPrice}`}</p>
+      <br />
+      <p>Object working as an Enum by using "as const"</p>
+      <p>{`${FifthPrices.applePrice} ${FifthPrices.bananaPrice} ${FifthPrices.peachPrice}`}</p>
+      <p>{`${JSON.stringify(Direction)}`}</p>
+
       {/* This won't work */}
       {/* <p>{`${JSON.stringify(constDirection)}`}</p> */}
+      <p>
+        Direction.Up and constDirection.Up work the same, displaying Direction
+        would work and it would show an object but constDirection won't work.
+      </p>
       <p>{`${JSON.stringify(Direction.Up)}`}</p>
       <p>{`${JSON.stringify(constDirection.Up)}`}</p>
     </div>
