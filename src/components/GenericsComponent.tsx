bd374c12412f76a4044446b5372a19844b17234a
MikeNotu <michael.bateman@globant.com>
2022-09-28 11:52:36 -0500

    Typescript Generics pt2

--- src/components/GenericsComponent.tsx
+++ src/components/GenericsComponent.tsx
@@ -9,10 +9,58 @@ const GenericsComponent = () => {
   const l2 = last(["a", "b", "c"]);
   //   const l2 = last<string>(["a", "b", "c"]);
 
+  const makeArr = <X, Y>(x: X, y: Y): [X, Y] => {
+    return [x, y];
+  };
+
+  const v = makeArr(5, 6);
+  const v2 = makeArr("a", "b");
+  const v3 = makeArr<string | null, number>(null, 5);
+
+  const makeFullName = (obj: { firstName: string; lastName: string }) => {
+    return {
+      ...obj,
+      fullName: obj.firstName + " " + obj.lastName,
+    };
+  };
+
+  const v4 = makeFullName({ firstName: "bob", lastName: "junior" });
+  // This won't work
+  //   const v4 = makeFullName({ firstName: "bob", lastName: "junior", age: 15 });
+
+  const makeFullNameGenerics = <
+    T extends { firstName: string; lastName: string }
+  >(
+    obj: T
+  ) => {
+    return {
+      ...obj,
+      fullName: obj.firstName + " " + obj.lastName,
+    };
+  };
+
+  const v5 = makeFullNameGenerics({
+    firstName: "bob",
+    lastName: "junior",
+    age: 15,
+  });
+
   return (
     <div>
+      <p>Basic Generics use case.</p>
       <p>{l}</p>
       <p>{l2}</p>
+      <br />
+      <p>Multiple Generics.</p>
+      <p>{v}</p>
+      <p>{v2}</p>
+      <p>{v3}</p>
+      <p>Objects must contain specific properties.</p>
+      <p>{JSON.stringify(v4)}</p>
+      <p>
+        Objects must contain specific properties, but can have more properties.
+      </p>
+      <p>{JSON.stringify(v5)}</p>
     </div>
   );
 };
